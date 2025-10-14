68567d93f49fe7a75c11a7252f8c09a0d2319f85
Porrith Suong <porrith.suong@gmail.com>
2025-10-13 21:27:57 -0400

    Ignores coverage results. Adds queue tests.

--- InitialPrefabs.Collections.Tests/NoAllocQueueTests.cs
+++ InitialPrefabs.Collections.Tests/NoAllocQueueTests.cs
@@ -13,6 +13,19 @@ namespace InitialPrefabs.Collections.Tests {
                 Assert.That(q.Count == 0, "No elements should be added to the queue.");
 
                 for (int i = 0; i < 10; i++) {
+                    Assert.That(q.TryEnqueue(i), $"Failed to enqueue {i}");
+                    Assert.That(q.Count == i + 1);
+                }
+
+                Assert.That(q.TryEnqueue(10) == false, "10 should not be enqueued.");
+
+                int count = 0;
+                while (!q.IsEmpty) {
+                    Assert.That(q.TryPeek(out int element), "Failed to view the head of the queue.");
+                    Assert.That(element == count, $"The head of the queue was not the same as {count}");
+                    Assert.That(q.TryDequeue(out element) && element == count);
+                    count++;
+                    Assert.That(q.Count == q.Capacity - count);
                 }
             });
         }
