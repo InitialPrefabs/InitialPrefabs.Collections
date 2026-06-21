5ac398f85420e04cc4b336f935e5fadb72a89711
Porrith Suong <porrith.suong@gmail.com>
2026-06-21 10:11:45 -0400

    Adds docstrings

--- InitialPrefabs.Collections/NoAllocPriorityQueue.cs
+++ InitialPrefabs.Collections/NoAllocPriorityQueue.cs
@@ -2,10 +2,26 @@ using System;
 using System.Collections.Generic;
 
 namespace InitialPrefabs.Collections {
+
+    /// <summary>
+    /// A priority queue is a queue that allows each item stored an assigned priority. The priority determines
+    /// the order of service when an element is dequed. They can serve the highest priority or lowest priority 
+    /// based on the implementation of <see cref="TComparer"/>.
+    /// </summary>
+    /// <typeparam name="TItem">The element to store</typeparam>
+    /// <typeparam name="TPriority">The assigned priority</typeparam>
+    /// <typeparam name="TComparer">The comparison function to determine the service of order.</typeparam>
     public ref struct NoAllocPriorityQueue<TItem, TPriority, TComparer> where TComparer : struct, IComparer<TPriority> {
+        /// <summary>
+        /// The total number of elements in the Queue.
+        /// </summary>
         public int Count {
             get => count;
         }
+
+        /// <summary>
+        /// The maximum number of elements that can be stored in the queue.
+        /// </summary>
         public int Capacity {
             get => items.Length;
         }
@@ -68,6 +84,12 @@ namespace InitialPrefabs.Collections {
             }
         }
 
+        /// <summary>
+        /// Attempts to enqueue an item to the queue.
+        /// </summary>
+        /// <param name="item">The element to store.</param>
+        /// <param name="priority">The assigned priority of the element,</param>
+        /// <returns>True, if successfully stored.</returns>
         public bool TryEnqueue(TItem item, TPriority priority) {
             if (count >= Capacity) {
                 return false;
@@ -80,7 +102,13 @@ namespace InitialPrefabs.Collections {
             HeapifyUp(index);
             return true;
         }
-
+        
+        /// <summary>
+        /// Attempts to dequeue an item from the queue.
+        /// </summary>
+        /// <param name="item">The element in the queue</param>
+        /// <param name="priority">The associated priority of the queue</param>
+        /// <returns>True, if there are elements stored in the queue.</returns>
         public bool TryDequeue(out TItem item, out TPriority priority) {
             if (count == 0) {
                 item = default!;
@@ -103,6 +131,12 @@ namespace InitialPrefabs.Collections {
             return true;
         }
 
+        /// <summary>
+        /// Attempts to look at the head of queue based on the assigned priority.
+        /// </summary>
+        /// <param name="item">The element at the head of the queue.</param>
+        /// <param name="priority">The associated priority of the queue.</param>
+        /// <returns>True, if there is an element at the end.</returns>
         public bool TryPeek(out TItem item, out TPriority priority) {
             if (count == 0) {
                 item = default;
